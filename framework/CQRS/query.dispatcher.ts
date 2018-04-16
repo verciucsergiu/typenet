2b366b88b9e23a466f3b20b41c6b212188d1828e
Sergiu Verciuc <v_sergiu2009@yahoo.com>
2018-04-16 20:17:12 +0300

    Typo

--- framework/CQRS/query.dispatcher.ts
+++ framework/CQRS/query.dispatcher.ts
@@ -5,14 +5,14 @@ import { CQRSContainer } from './cqrs.container';
 @Injectable()
 export class QueryDispatcher {
     public dispatch<TQuery extends IQuery, TResult extends IQueryResult>(query: TQuery): TResult {
-        const queryHandlerInsance = CQRSContainer.getQueryHandler(query['constructor']);
-        const result: TResult = queryHandlerInsance['retrieve'](query);
+        const queryHandlerInstance = CQRSContainer.getQueryHandler(query['constructor']);
+        const result: TResult = queryHandlerInstance['retrieve'](query);
         return result;
     }
 
     public async dispatchAsync<TQuery extends IQuery, TResult extends IQueryResult>(query: TQuery): Promise<TResult> {
-        const queryHandlerInsance = CQRSContainer.getQueryHandler(query['constructor']);
-        const result: TResult = await queryHandlerInsance['retrieve'](query);
+        const queryHandlerInstance = CQRSContainer.getQueryHandler(query['constructor']);
+        const result: TResult = await queryHandlerInstance['retrieve'](query);
         return result;
     }
 }
