1cec236702934254b4044c8af25d74b0b6a729ab
Sergiu Verciuc <v_sergiu2009@yahoo.com>
2019-06-26 11:01:16 +0300

    chat

--- samples/02 - chat/server/src/chat-hub/chat.hub.ts
+++ samples/02 - chat/server/src/chat-hub/chat.hub.ts
@@ -1,10 +1,28 @@
 import { WebSocketHub, Subscribe } from "@typenet/web-socket";
 import { Socket } from 'socket.io';
+import { messages } from './messages';
+
 @WebSocketHub()
 export class ChatHub {
 
-    @Subscribe('joined')
+    @Subscribe('join')
     public connect(socket: Socket, message: any): void {
         socket.broadcast.emit('user-joined', { username: message.username });
+        socket.emit('welcome', messages.welcome);
+    }
+
+    @Subscribe('message')
+    public messageReceived(socket: Socket, message: any): void {
+        socket.broadcast.emit('user-message', message);
+    }
+
+    @Subscribe('typing')
+    public typing(socket: Socket, message: any): void {
+        socket.broadcast.emit('user-typing', { username: message.username });
+    }
+
+    @Subscribe('left')
+    public userLeft(socket: Socket, message: any): void {
+        socket.broadcast.emit('user-left', { username: message.username });
     }
 }
